182214bf58c517e7bf986c4afb9406a87415d70d
Mikhail Hasso <mihasso@protonmail.com>
2021-03-14 11:29:20 +0200

    outline

--- contracts/RFT.sol
+++ contracts/RFT.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol'; //Token standart for NFT, allows to manipulate some NFT token
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';  //Token standart for fungible token, allow to manipulate of die, buy, share
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract NFT is ERC20{
+//Refungible token
+contract RFT is ERC20{
  uint public icoSharePrice;
  uint public icoShareSupply;
- uint public icoEnd;
+ uint public icoEnd;  //If limited in time
  
  uint public nftId;
  IERC721 public nft;
  IERC20 public dai;
 
- address public admin;
+ address public admin; 
 
  constructor(
-   string memory _name,
+   string memory _name, //Name of refungible token
    string memory _symbol,
    address _nftAddress,
    uint _nftId,
@@ -38,7 +39,7 @@ contract NFT is ERC20{
  function startIco() external {
    require(msg.sender==admin, 'only admin');
    nft.transferFrom(msg.sender, address(this), nftId);
-   icoEnd = block.timestamp + 7 * 86400;
+   icoEnd = block.timestamp + 7 * 86400;  //1 week
  }
 
   function buyShare(uint shareAmount) external {
