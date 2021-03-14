e96f842aae7216cbf69858b59b8675c78cdf4928
Mikhail Hasso <mihasso@protonmail.com>
2021-03-14 14:16:33 +0200

    outline

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import '@openzeppelin/contracts/token/ERC721/IERC721.sol'; //Token standart for NFT, allows to manipulate some NFT token
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol'; //Token standart for NFT, allows to manipulate some NFT token
 
 contract NFT is ERC721{
     constructor(
         string memory name,
         string memory symbol
-    ) public
+    ) public 
     ERC721(name, symbol){
 
     }
